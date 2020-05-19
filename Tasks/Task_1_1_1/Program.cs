15025f009a6bc743b97efdf7a9503d51b4a828f9
Eric X <eric-1@yandex.ru>
2020-05-19 23:23:33 +0400

    Task 1_1_1

---
+++ Tasks/Task_1_1_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a_str, b_str;
+            Int16 a, b;
+            Console.Write("Расчет площади прямоугольника:\na = ");
+            a_str = Console.ReadLine();
+            Int16.TryParse(a_str, out a);
+            Console.Write("b = ");
+            b_str = Console.ReadLine();
+            Int16.TryParse(b_str, out b);
+            if ( a <= 0 || b <= 0 )
+            {
+                Console.WriteLine("Введены некорректные значения длины и ширины");
+                return;
+            }
+            Console.WriteLine("Площадь прямоугольника равна " + a * b);
+            return;
+        }
+    }
+}
