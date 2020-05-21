6e6edc79957e1deb89b29fc2111d967763a1f9e0
Eric X <eric-1@yandex.ru>
2020-05-21 08:17:31 +0400

    Optimizing task 1_1_6

--- Tasks/Task_1_1_6/Program.cs
+++ Tasks/Task_1_1_6/Program.cs
@@ -15,49 +15,32 @@ namespace Task_1_1_6 // FONT ADJUSTMENT
             FontStyles textParam = 0;
             while ( key != 0 )
             {
-                Console.Write("Параметры надписи: ");
-                if (textParam.HasFlag(FontStyles.None))
-                    Console.Write(FontStyles.None + " ");
-                if (textParam.HasFlag(FontStyles.Bold))
-                    Console.Write(FontStyles.Bold + " ");
-                if (textParam.HasFlag(FontStyles.Italic))
-                    Console.Write(FontStyles.Italic + " ");
-                if (textParam.HasFlag(FontStyles.Underline))
-                    Console.Write(FontStyles.Underline + " ");
+                Console.Write("Параметры надписи: " + textParam);
                 Console.WriteLine();
 
-                Console.WriteLine("Введите:\n\t0: Exit\n\t1: Bold\n\t2: Italic\n\t3: Underline\n");
+                Console.WriteLine("Введите:\n\t0: Exit\n\t1: Bold\n\t2: Italic\n\t3: Underline");
                 c_key = Console.ReadKey().KeyChar;
                 byte.TryParse(c_key.ToString(), out key);
                 Console.WriteLine();
 
                 switch (key) {
                     case 0:
+                        Console.WriteLine("--- Выходим ---");
                         break;
                     case 1:
-                        toggleParam(ref textParam, FontStyles.Bold);
+                        textParam ^= FontStyles.Bold;
                         break;
                     case 2:
-                        toggleParam(ref textParam, FontStyles.Italic);
+                        textParam ^= FontStyles.Italic;
                         break;
                     case 3:
-                        toggleParam(ref textParam, FontStyles.Underline);
+                        textParam ^= FontStyles.Underline;
                         break;
                     default:
                         Console.WriteLine("Некорректный ввод");
                         break;
                 }
             }
-        }
-
-        //bool getParamState(FontStyles param) { }
-
-        static void toggleParam(ref FontStyles param, FontStyles style)
-        {
-            if ( param.HasFlag(style) )
-                param = (byte)param - style;
-            else
-                param = (byte)param + style;
         }
 
         [Flags]
