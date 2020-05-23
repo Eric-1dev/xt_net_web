a7718f0435a817ead0bdd50d919c1ad952636435
Eric X <eric-1@yandex.ru>
2020-05-23 17:09:00 +0400

    Add method printArr for 3d-array in task 1_1_8

--- Tasks/Task_1_1_8/Program.cs
+++ Tasks/Task_1_1_8/Program.cs
@@ -13,7 +13,7 @@ namespace Task_1_1_8 // NO POSITIVE
             string str;
             uint n;
 
-            Console.Write("Введите число элементов: ");
+            Console.Write("Введите размер массива: ");
             str = Console.ReadLine();
             uint.TryParse(str, out n);
 
@@ -25,7 +25,10 @@ namespace Task_1_1_8 // NO POSITIVE
             for (uint i = 0; i < n; i++)
                 for (uint j = 0; j < n; j++)
                     for (uint k = 0; k < n; k++)
-                        arr[i, j , k] = rnd.Next();
+                        arr[i, j , k] = rnd.Next(-1000, 1000);
+
+            Console.WriteLine("-------- Исходный массив: --------");
+            printArr<int>(arr, ref n, ref n, ref n);
 
             // Пробегаемся по массиву и заменяем положительные элементы нулями
             for (uint i = 0; i < n; i++)
@@ -33,6 +36,28 @@ namespace Task_1_1_8 // NO POSITIVE
                     for (uint k = 0; k < n; k++)
                         if (arr[i, j, k] > 0)
                             arr[i, j, k] = 0;
+
+            Console.WriteLine("-------- Новый массив: --------");
+            printArr<int>(arr, ref n, ref n, ref n);
+        }
+
+        // Вывод трехмерного массива размерностью x y z, в виде z таблиц размером x * y
+        // Метод универсальный, для массивов разных типов и размерностей. Все переменные передаем
+        // ссылками, чтобы не плодить одинаковые данные в памяти.
+        static void printArr<T>(T[,,] array, ref uint x, ref uint y, ref uint z)
+        {
+            for (int i = 0; i < z; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    for (int k = 0; k < x; k++)
+                    {
+                        Console.Write(array[i, j, k] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
