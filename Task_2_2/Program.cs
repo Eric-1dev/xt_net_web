9989d4a5a7347e1ee2c550307f5491c00bad60a8
Eric X <eric-1@yandex.ru>
2020-06-04 00:02:31 +0400

    Task_2_2_1 processing

--- Task_2_2/Program.cs
+++ Task_2_2/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Task_2_2
 {
@@ -40,7 +41,6 @@ namespace Task_2_2
                             exit = true;
                             break;
                     }
-                    //snake.Rotation(key.Key);
                 }
             }
         }
@@ -48,18 +48,39 @@ namespace Task_2_2
 
     class GameObject
     {
+        private char[] _view;
+        static private Random _rnd = new Random();
         public int X { get; set; }
         public int Y { get; set; }
-        public char[] View { get; set; }
+        public char[] View 
+        {
+            get => _view;
+            set
+            {
+                _view = value;
+                Draw();
+            }
+        }
         public int Width { get; private set; }
         public int Height { get; private set; }
-        public GameObject(int x, int y, int width, int height)
+        public GameObject(int width, int height)
         {
-            X = x;
-            Y = y;
             Width = width;
             Height = height;
+            SetRndCoords();
+            FitToEdge();
+            for (int i = 0; i < allGameObjects.Count; i++)
+            {
+                if (this is Character)
+                    if ((this as Character).Cross(allGameObjects[i]))
+                        SetRndCoords();
+            }
             allGameObjects.Add(this);
+        }
+        private void SetRndCoords()
+        {
+            X = _rnd.Next(0, Game.X - Width);
+            Y = _rnd.Next(0, Game.Y - Height);
         }
         private void Draw_Clear(DRAW_CLEAR flag)
         {
@@ -73,22 +94,30 @@ namespace Task_2_2
                     else
                         Console.Write(' ');
                 }
-                Console.SetCursorPosition(X, Y + 1 + i);
-
+                Console.SetCursorPosition(X, Y + i + 1);
             }
+        }
+        protected void FitToEdge()
+        {
+            if (X < 1) X = 1;
+            if (X > Game.X - Width) X = Game.X - Width;
+            if (Y < 1) Y = 1;
+            if (Y > Game.Y - Height) Y = Game.Y - Height;
         }
         public void Draw() { Draw_Clear(DRAW_CLEAR.DRAW); }
         public void Clear() { Draw_Clear(DRAW_CLEAR.CLEAR); }
+        public void Remove()
+        {
+            Clear();
+            allGameObjects.Remove(this);
+        }
         
         static public List<GameObject> allGameObjects = new List<GameObject>();
     }
 
     class Character : GameObject
     {
-        public Character(int x, int y, int width, int height) : base(x, y, width, height)
-        {
-
-        }
+        public Character(int width, int height) : base(width, height) { }
         public void Move(MOVE direction)
         {
             int old_x = X;
@@ -111,10 +140,7 @@ namespace Task_2_2
 
             }
             // Не пересекаем границы поля
-            if (X < 1) X = 1;
-            if (X > Game.X - 3) X = Game.X - 3;
-            if (Y < 1) Y = 1;
-            if (Y > Game.Y - 3) Y = Game.Y - 3;
+            FitToEdge();
             // --------------------------
 
             // Пересекаемся с другими объектами на карте
@@ -122,23 +148,20 @@ namespace Task_2_2
             {
                 if (this.Cross(allGameObjects[i]) && this != allGameObjects[i])
                 {
-                    if (this is Player && allGameObjects[i] is Enemy) // Пересечение с врагом
+                    if (this is Player && allGameObjects[i] is Enemy) // Пересечение игрока с врагом
                     {
                         X = old_x; Y = old_y;
-                        (this as Player).HP--;
-                        if ((this as Player).HP < 1)
-                        {
-                            (this as Player).HP = 0;
-                            Console.SetCursorPosition(Game.X / 2 - 6, Game.Y / 2);
-                            Console.WriteLine("Game over!!!");
-                        }
-                        (this as Player).UpdateHPBar();
+                        (this as Player).Damage();
                     }
-                    else if (this is Player && allGameObjects[i] is Forest) // Пересечение со съедобным объектом
+                    else if (this is Player && allGameObjects[i] is Medkit) // Пересечение игрока с аптечкой
                     {
-                        allGameObjects[i].Clear();
-                        allGameObjects.Remove(allGameObjects[i]);
+                        (this as Player).Heal((allGameObjects[i] as Medkit).HEALCOUNT);
+                        allGameObjects[i].Remove();
                         i++;
+                    }
+                    else if (this is Player && allGameObjects[i] is Key)
+                    {
+                        Game.Stop(true);
                     }
                     else
                         X = old_x; Y = old_y;
@@ -157,24 +180,49 @@ namespace Task_2_2
 
     class Player : Character
     {
-        public int HP { get; internal set; }
-        public Player(int x, int y, int hp) : base(x, y, 3, 3)
+        private int _max_hp;
+        private int _hp;
+        public int HP
+        {
+            get => _hp;
+            internal set
+            {
+                _hp = value;
+                if (_hp > _max_hp)
+                    _hp = _max_hp;
+                UpdateHPBar();
+            }
+        }
+        public Player(int max_hp) : base(3, 3)
         {
             //string str = "bod" + ")╨(" + "\u250C-\u2510";
-            HP = hp;
-            string str = "b_d" + ")╨(" + "\u2510-\u2510".ToCharArray();
+            HP = _max_hp = max_hp;
+            string str = "b_d" + ")╨(" + "\u2510-\u2510";
             View = str.ToCharArray();
         }
-        public void UpdateHPBar()
+        private void UpdateHPBar()
         {
-            string hpBar = "HP: " + new string('|', HP) + " ";
+            string hpBar = " HP: " + new string('|', HP) + " ";
             Console.SetCursorPosition(2, 0);
             Console.Write(hpBar);
+        }
+        public void Heal(int count)
+        {
+            HP += count;
+        }
+        public void Damage()
+        {
+            HP--;
+            if (HP < 1)
+            {
+                HP = 0;
+                Game.Stop(false);
+            }
         }
     }
     class Enemy : Character
     {
-        public Enemy(int x, int y) : base(x, y, 3, 3)
+        public Enemy() : base(3, 3)
         {
             string str = "/-\\" + "|||" + "\\_/";
             View = str.ToCharArray();
@@ -182,19 +230,57 @@ namespace Task_2_2
     }
     class Forest : GameObject
     {
-        public Forest(int x, int y, int width, int height) : base(x, y, width, height)
+        public Forest(int width, int height) : base(width, height)
         {
             string str = new string('^', width * height);
             View = str.ToCharArray();
         }
     }
+    class Medkit : GameObject
+    {
+        public int HEALCOUNT { get; private set; }
+        public MED_TYPE MEDTYPE { get; private set; }
+        public Medkit(MED_TYPE medType) : base(medType == MED_TYPE.BIG ? 5 : 3, 3)
+        {
+            string str;
+            this.MEDTYPE = medType;
+            if (this.MEDTYPE == MED_TYPE.SMALL)
+            {
+                HEALCOUNT = 1;
+                str = "┌─┐" + "│+│" + "└─┘";
+            }
+            else
+            {
+                HEALCOUNT = 3;
+                str = "┌───┐" + "│+++│" + "└───┘";
+            }
+            View = str.ToCharArray();
+        }
+    }
+    class Key : GameObject
+    {
+        public Key() : base(3, 3)
+        {
+            string str = "┌─┐" + "│$│" + "└─┘";
+            View = str.ToCharArray();
+        }
+    }
     class Game
     {
+        private bool _run { get; set; }
         static public int X { get; private set; }
         static public int Y { get; private set; }
         public char Edge { get; set; } = '+';
+        static public Timer timer;
 
         private Player player;
+        private Key key;
+        private Enemy[] enemy = new Enemy[4];
+        private Medkit[] food_big = new Medkit[1];
+        private Medkit[] food_small = new Medkit[3];
+        private Forest[] forest = new Forest[4];
+
+        private Random rnd = new Random();
         public Game(int x, int y)
         {
             X = x;
@@ -205,33 +291,73 @@ namespace Task_2_2
 
             DrawEdge();
 
-            player = new Player(20, 30, 10);
-            player.Draw();
-            player.UpdateHPBar();
+            player = new Player(10);
+            key = new Key();
 
-            Enemy enemy = new Enemy(40, 30);
-            enemy.Draw();
+            for (int i = 0; i < enemy.Length; i++)
+            {
+                enemy[i] = new Enemy();
+            }
 
-            Forest forest = new Forest(25, 15, 3, 3);
-            forest.Draw();
+            for (int i = 0; i < food_big.Length; i++)
+            {
+                food_big[i] = new Medkit(MED_TYPE.BIG);
+            }
+
+            for (int i = 0; i < food_small.Length; i++)
+            {
+                food_small[i] = new Medkit(MED_TYPE.SMALL);
+            }
+
+            for (int i = 0; i < forest.Length; i++)
+            {
+                forest[i] = new Forest(rnd.Next(2, 10), rnd.Next(2, 10));
+            }
+            Start();            
         }
         private void DrawEdge()
         {
-            string str = new string(Edge, X);
-            Console.SetCursorPosition(2, 0);
+            string str = new string(Edge, X - 1);
+            Console.SetCursorPosition(1, 0);
             Console.Write(str);
-            Console.SetCursorPosition(0, Y);
+            Console.SetCursorPosition(1, Y);
             Console.Write(str);
             for (int i = 0; i < Y; i++)
             {
                 Console.SetCursorPosition(0, i);
-                Console.WriteLine(Edge);
+                Console.Write(Edge);
+                Console.SetCursorPosition(X, i);
+                Console.Write(Edge);
             }
+        }
+        public bool isRun() => _run;
+        private void Start() { 
+            _run = true;
+            timer = new Timer(TickTak, null, 0, 200);
+        }
+        static public void Stop(bool win)
+        {
+            Console.SetCursorPosition(Game.X / 2 - 6, Game.Y / 2);
+            if (win)
+                Console.WriteLine("You WIN!!!");
+            else
+                Console.WriteLine("Game over!!!");
+            timer.Dispose();
         }
 
         public void MovePlayer(MOVE direction)
         {
-            player.Move(direction);
+            if (isRun())
+                player.Move(direction);
+        }
+        static void TickTak(object obj)
+        {
+            Random rnd = new Random();
+            foreach (var gameObj in GameObject.allGameObjects)
+            {
+                if (gameObj is Enemy)
+                    (gameObj as Enemy).Move((MOVE)rnd.Next((int)MOVE.LEFT - 1, (int)MOVE.DOWN) + 1);
+            }
         }
     }
     enum MOVE
@@ -239,13 +365,18 @@ namespace Task_2_2
         LEFT,
         RIGHT,
         UP,
-        DOWN,
-        STAY
+        DOWN
     }
 
     enum DRAW_CLEAR
     {
         DRAW,
         CLEAR
+    }
+
+    enum MED_TYPE
+    {
+        SMALL,
+        BIG
     }
 }
