3ad88e3cfdeabe4dfba53db43e3860b06ebaf4e9
Eric X <eric-1@yandex.ru>
2020-07-01 17:56:15 +0400

    Task_3_1_1 refactoring and anticostyling

--- Task_3_1/Task_3_1_1/Program.cs
+++ Task_3_1/Task_3_1_1/Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime;
-using System.Runtime.ExceptionServices;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_3_1_1
 {
@@ -14,62 +8,216 @@ namespace Task_3_1_1
     {
         static void Main(string[] args)
         {
-            ExtendedList<Human> extendedList = new ExtendedList<Human>();
+            int n, divider;
+            CircularLinkedList<Human> circularList = new CircularLinkedList<Human>();
 
-            for (int i = 1; i <= 3; i++)
+            Console.WriteLine("Введите количество человек:");
+            int.TryParse(Console.ReadLine(), out n);
+            if ( n < 0 )
             {
-                extendedList.AddLast(new Human("Вася-" + i));
+                Console.WriteLine("N должно быть положительным.");
+                return;
+            }
+            Console.WriteLine("Введите, какой по счету человек будет вычеркнут каждый раунд:");
+            int.TryParse(Console.ReadLine(), out divider);
+            if (divider > n)
+            {
+                Console.WriteLine("По условию задачи, данное число должо быть меньше общего числа людей.");
+                return;
             }
 
-            for (int i = 0; i < 12; i++)
+            // Создаём "человеков"
+            for (int i = 1; i <= n; i++)
             {
-                if (extendedList.Step())
-                    extendedList.RemoveCurrent();
-                Console.WriteLine(extendedList.CurrentPos.Value);
+                circularList.Add(new Human("Вася"));
             }
-        }
-    }
-}
 
-// Расширим функционал связного списка, чтобы реализовать возможность кольцевого обхода
-public class ExtendedList<T> : LinkedList<T>
-{
-    public LinkedListNode<T> CurrentPos { get; private set; }
+            // Создаём ссылку на первый элемент кольцевого списка
+            CircularLinkedListNode<Human> elem = circularList.First;
 
-    public bool Step()
-    {
-        if (Count != 0 && CurrentPos == null) // Заглушка, если текущая позиция не определена
-            CurrentPos = First;
-        else if (Count == 1)
-            return false;
-        else
-            CurrentPos = ReferenceEquals(CurrentPos, Last) ? First : CurrentPos.Next;
-        return true;
-    }
-    public bool RemoveCurrent()
-    {
-        LinkedListNode<T> _temp = CurrentPos;
-        if (Step() != null )
-        {
-            Remove(_temp);
-            return true;
+            int round = 1;
+            int j = 1;
+            // Бегаем по списку до тех пор, пока он не уменьшится до размеров делителя
+            while (circularList.Count >= divider)
+            {
+                if ( j % divider == 0 )
+                {
+                    Console.WriteLine($"Раунд {round}. Вылетает участник {elem.Value.Name} с номером {elem.Value.Number}. \tИгроков осталось: {circularList.Count}");
+                    round++;
+                    circularList.Remove(elem.Value);
+                }
+                elem = elem.Next;
+                j++;
+            }
+            Console.WriteLine("Игра окончена. Невозможно вычеркнуть больше людей.");
         }
-        return false;
     }
 }
 
 public class Human
 {
-    static private int _numbers = 1;
+    private static int _numbers = 1;
     public int Number { get; private set; }
     public string Name { get; private set; }
     public Human(string name)
     {
-        Number = _numbers++;
         Name = name;
+        Number = _numbers++;
     }
-    public override string ToString()
+    public override bool Equals(object obj)
     {
-        return Name + " " + Number;
+        return Name == (obj as Human).Name && Number == (obj as Human).Number;
     }
+}
+
+
+/* 
+ * Задачу можно было решить с помощью встроенного LinkedList,
+ * но с целью лучшего понимания работы коллекций
+ * напишем собственную коллекцию, представляющую из себя
+ * кольцевой односвязный список.
+ */
+public class CircularLinkedList<T> : ICollection<T>, IEnumerable<T>
+{
+    public CircularLinkedList()
+    {
+        Count = 0;
+    }
+
+    public int Count { get; private set; }
+    public CircularLinkedListNode<T> First { get; private set; }
+    public bool IsReadOnly => false;
+
+    public void Add(T item)
+    {
+        CircularLinkedListNode<T> newNode = new CircularLinkedListNode<T>(item);
+        newNode.List = this;
+        if (Count == 0)
+        {
+            First = newNode;
+            newNode.Next = newNode;
+        }
+        else
+        {
+            CircularLinkedListNode<T> lastNode = First.Next;
+
+            // Пробегаемся по списку в поисках последнего элемента
+            while(lastNode.Next != First)
+            {
+                lastNode = lastNode.Next;
+            }
+            lastNode.Next = newNode;
+            newNode.Next = First;
+        }
+        Count++;
+    }
+
+    public void Clear()
+    {
+        First = null; // Ломаем ссылку первый элемент. Остальное вычистит Garbage Collecotor (вроде бы)
+        Count = 0;
+    }
+
+    public bool Contains(T item)
+    {
+        foreach (var elem in this)
+        {
+            if (elem.Equals(item))
+                return true;
+        }
+        return false;
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(T item)
+    {
+        CircularLinkedListNode<T> curNode = First;
+        CircularLinkedListNode<T> prevNode;
+        do
+        {
+            prevNode = curNode;
+            curNode = curNode.Next;
+            if (curNode.Value.Equals(item))
+            {
+                prevNode.Next = curNode.Next;
+                Count--;
+                return true;
+            }
+        }
+        while (curNode != First);
+        return false;
+    }
+
+    public IEnumerator<T> GetEnumerator() => new Enumerator(this);
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public struct Enumerator : IEnumerator<T>
+    {
+        private CircularLinkedList<T> _list;
+        private CircularLinkedListNode<T> _curNode;
+
+        public Enumerator(CircularLinkedList<T> list)
+        {
+            _list = list;
+            _curNode = null;
+        }
+        public T Current { 
+            get
+            {
+                return _curNode.Value;
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            _curNode = null;
+        }
+
+        public bool MoveNext()
+        {
+            // Если _curNode пока не определен (первый вызов MoveNext), ставим его в начало списка и возращаемся
+            if (_curNode == null)
+            {
+                _curNode = _list.First;
+                return true;
+            }
+
+            // Если дошли по кругу до первого элемента - значит полностью обошли список
+            if (_curNode.Next == _list.First)
+                return false;
+            else
+                _curNode = _curNode.Next;
+            return true;
+        }
+
+        public void Reset()
+        {
+            _curNode = null;
+        }
+    }
+}
+
+// Элемент нашей коллекции
+public class CircularLinkedListNode<T>
+{
+    public CircularLinkedListNode(T value)
+    {
+        Value = value;
+        Next = null;
+    }
+
+    public T Value { get; set; }
+
+    public CircularLinkedListNode<T> Next { get; internal set; }
+    public CircularLinkedList<T> List { get; internal set; }
 }
