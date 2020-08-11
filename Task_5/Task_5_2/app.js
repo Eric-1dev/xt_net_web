6dba922b555dc1cdb362bcb6041745438356f426
Eric X <eric-1@yandex.ru>
2020-08-12 00:28:03 +0400

    Starting Task_5_2

---
+++ Task_5/Task_5_2/app.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+
+var str = '3.5 + 4 * 10 - 5.3 / 5 =';
+
+var numbers = [];
+var operators = [];
+
+var regNums = /\s*[\-\+\/\*=]\s*/;
+
+var regOperators = /[\d\.=\s]+/;
+
+var regCheckInput = /\s*(\d+?(\.{1})?\d*\s*[\+\-\/\*]\s*)+\d+?(\.{1})?\d*\s*=\s*/;
+
+if (str.replace(regCheckInput, '') == "")
+    console.log("Correct string");
+else
+    console.log("Incorrect string");
+
+numbers = str.split(regNums);
+operators = str.split(regOperators);
+
+console.log(str);
+console.log(numbers);
+console.log(operators);
